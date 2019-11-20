88abcb9fc298466b900a4c8aab342144580e805c
AD calcium milk <c180030668@gmail.com>
2019-11-20 15:40:59 +0800

    Leetcode no.14 SB empty-input param

--- Source/Leetcode/14. 最长公共前缀.c
+++ Source/Leetcode/14. 最长公共前缀.c
@@ -1,27 +1,24 @@
+#include<stdlib.h>
 #include"../macro.h"
-#include<memory.h>
-#define true 1 
+
+/* 竖排扫描 */
 char * longestCommonPrefix1(char ** strs, int strsSize)
 {
-	int len=0;
-	char *str=NULL;
+	int len = 0;
 	
-	if(strsSize==0)  return "";
-	while(true){
-		int rc=0;
-		char common=strs[0][len],tmp=0;
-
-		for(int i=0;i<strsSize;i++){
+	if(strsSize == 0)  return "";
+	
+	for(char tmpo,tmp ;strs[0][len] ;len++){
+		tmpo = strs[0][len];
+		for(int i=0 ;i<strsSize ;i++){
 			tmp = strs[i][len];
-			if(tmp=='\0' || tmp!=common){
-				rc=1;
-				break;
-			}
+			if(tmp == '\0' || tmp != tmpo)
+				goto OUT;
 		}
-		if(rc)	break;
-		len++;
 	}
-
+	
+	char *str
+OUT:
 	str = mlloc(char,len+1);
 	memcpy(str,strs[0],len);
 	str[len]='\0';
@@ -29,42 +26,45 @@ char * longestCommonPrefix1(char ** strs, int strsSize)
 	return str;
 }
 
+/* 归并 */
 char * merge(char *a,char *b)
 {
-	char *str=NULL;
-	int i=0;
-	for(i=0;;i++){
-		if(a[i]!=b[i] || a[i]=='\0')
+    if(!a || !b)    return NULL;
+    
+	int len;
+    
+	for(  ;  ;len++){
+		if(a[len]=='\0' || a[len]!=b[len])
 			break;
-	}
-	str = mlloc(char,i+1);
-	memcpy(str,a,i);
-	str[i]='\0';
+    }
+	char *str = mlloc(char,len+1);
+	memcpy(str,a,len);
+	str[len]='\0';
     
 	return str;
 }
 char * mergecommon(char **str,int low, int high)
 {
+	char *string;
 	if(low > high)	return NULL;
-	if(low == high)	return str[low];
-	if(high == low+1)	return merge(str[low],str[high]);
-	
+	if(low == high){					//保证left和right都是函数内部malloc分配的内存，可以释放
+		string = mlloc(char,strlen(str[low])+1);
+		memcpy(string,str[low],strlen(str[low])+1);
+		return string;
+	}		
+	if(high == low+1)	return merge(str[low],str[high]); //这里并非再为了保证malloc，而是为了避免
+																		//下方再次递归的开销(包括lowhigh变量相等重新malloc)
+	char *left,*right;		
 	int mid = (low+high)/2;
-	char *left,*right;
-	left = mergecommon(str,low,mid);
+	left = mergecommon(str,low,mid);	//递归执行到此可以保证变量指向的是新malloc分配的而非str递归参数
 	right = mergecommon(str,mid+1,high);
-
-	return merge(left,right);
+	
+	string = merge(left,right);
+	free(left); free(right);
+	return string;
 }
-char * longestCommonPrefix2(char ** strs, int strsSize)
+char * longestCommonPrefix(char ** strs, int strsSize)
 {
 	if(strsSize==0) return "";
 	return mergecommon(strs,0,strsSize-1);
-}
-
-int main()
-{
-	char * strs[] = {"leets","leetcode","leetside","lees","let"};
-	char *str = longestCommonPrefix2(strs,5);
-	prt(%s\n,str);
 }
