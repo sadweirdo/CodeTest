5be329167013026c30b2f6c2d20b85b5ff6d07db
AD calcium milk <c180030668@gmail.com>
2019-11-24 23:07:47 +0800

    Leetcode no.26

---
+++ Source/Leetcode/26. 删除排序数组中的重复项_简单.c
@@ -0,0 +1,25 @@
+#include"../macro.h"
+/* 只需修改数组前列元素使其均不相等即可，例如 0 0 0 这类数组就不必修改直接返回 */ 
+int removeDuplicates(int* nums, int numsSize){
+    if(!numsSize)   return 0;
+    
+    int index=0;
+    for(int i=1;i<numsSize;i++){
+        if(nums[i]!=nums[index])
+            nums[++index] = nums[i];
+    }
+    return index+1;
+}
+/* 个人最初版 */
+int removeDuplicates(int* nums, int numsSize){
+    if(!numsSize)   return 0;
+    
+    int index=0;//最初定义了prev = nums[index]
+    int count = 1;
+    for(int i=1;i<numsSize;i++){
+        if(nums[i]!=nums[index])   continue;
+        nums[++index] = nums[i];
+        count++;
+    }
+    return count;
+}
