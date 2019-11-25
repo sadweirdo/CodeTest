1ffdbd2328543dcb6918276ffd227930c1b81c50
AD calcium milk <c180030668@gmail.com>
2019-11-25 11:30:14 +0800

    Leetcode no.27

---
+++ Source/Leetcode/27. 移除元素_简单.c
@@ -0,0 +1,11 @@
+#include"../macro.h"
+int removeElement(int* nums, int numsSize, int val){
+    if(numsSize == 0)   return 0;
+    int i=0,tail=numsSize-1;
+    while(i<=tail){
+        if(nums[i]==val)
+            nums[i] = nums[tail--];
+        else    i++;
+    }
+    return i;
+}
